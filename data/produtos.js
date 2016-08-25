9729f0489c844eb516de5f1ba4124645ec38d150
murilo140891 <murilo140891@gmail.com>
2016-08-25 06:48:25 -0300

    Add files via upload

--- data/produtos.js
+++ data/produtos.js
@@ -1,1 +1,160 @@
+var listProdutos = [
+{
+"titulo":"Cesta de Chocolate 01",
+"preco":"R$ 27,00",
+"image":"http://res.cloudinary.com/murilofrizanco/image/upload/v1471810664/cesta-01_lmmxlt.jpg",
+"descricao":`
+Uma cesta de presentes ideal para presentar quem a gente gosta.
 
+* 1x Cookies
+* 1x Alfajor
+* 4 Trufas 30g
+`
+},
+
+{
+"titulo":"Cesta de Chocolate 02",
+"preco":"R$ 110,50",
+"image":"http://res.cloudinary.com/murilofrizanco/image/upload/v1471810625/cesta-02_k7hxdv.jpg",
+"descricao":`
+Uma cesta de presentes ideal para presentar quem a gente gosta.
+
+* 1x Caixa Bendito Cacao Sortidos 150g
+* 1x Bytes Wafer Avelã
+* 1x Montebello Maracujá
+* 1x Sintonia Wafer
+* 5x Trufas 30g
+`
+},
+
+{
+"titulo":"Vinho + Sommelier",
+"preco":"R$ 85,00",
+"image":"http://res.cloudinary.com/murilofrizanco/image/upload/v1471810670/vinho-sommelier_g5kjnh.jpg",
+"descricao":`
+Uma cesta de presentes ideal para presentar quem a gente gosta.
+
+* 1x Vinho Casa Valduga
+* 1x Sommelier
+`
+},
+
+{
+"titulo":"Vinho + Chocolate",
+"preco":"R$ 51,00",
+"image":"http://res.cloudinary.com/murilofrizanco/image/upload/v1471810669/vinho-02_v4znxf.jpg",
+"descricao":`
+Uma cesta de presentes ideal para presentar quem a gente gosta.
+
+* 1x Vinho Casa Valduga
+* 1x Barra La Creme 13,5g
+* 1x Alfajor
+* 1x Trufa Artesanal 30g
+* 4x Trufas 30g
+`
+},
+
+{
+"titulo":"Gourmet Rosê",
+"preco":"R$ 105,40",
+"image":"http://res.cloudinary.com/murilofrizanco/image/upload/v1471810658/cesta-gourmet-rose_hqazao.jpg",
+"descricao":`
+Uma cesta de presentes ideal para presentar quem a gente gosta.
+
+* 1x Gourmet Rosê
+* 1x Barra Sabor Morango 100g
+* 1x Barra ChocoBeauty 13,5g
+* 1x Pão de Mel
+* 1x Alfajor
+* 1x Trufa Artesanal 30g
+* 4x Trufas 30g
+`
+},
+
+{
+"titulo":"Vinho + Amêndoas",
+"preco":"R$ 105,40",
+"image":"http://res.cloudinary.com/murilofrizanco/image/upload/v1471810655/vinho-bendito-cacao-amendoa_mcmntl.jpg",
+"descricao":`
+Uma cesta de presentes ideal para presentar quem a gente gosta.
+
+* 1x Vinho Casa Valduga
+* 1x Drageados Amêndoas com Chocolate 70% 150g
+* 7x Trufas 30g
+`
+},
+
+{
+"titulo":"Presente 01",
+"preco":"R$ 35,00",
+"image":"http://res.cloudinary.com/murilofrizanco/image/upload/v1471810655/presente-01_t4jtk8.jpg",
+"descricao":`
+Uma cesta de presentes ideal para presentar quem a gente gosta.
+
+* 1x Vinho Casa Valduga
+* 1x Cookies
+* 1x Pão de Mel
+* 1x Barra 13,5g
+* 2x Trufas 30g
+* 2x Bombons
+`
+},
+
+{
+"titulo":"Presente Simples 01",
+"preco":"R$ 15,50",
+"image":"http://res.cloudinary.com/murilofrizanco/image/upload/v1471810649/presente-04_whsv1o.jpg",
+"descricao":`
+Uma cesta de presentes ideal para presentar quem a gente gosta.
+
+* 5x Trufas 30g
+`
+},
+
+{
+"titulo":"Cesta de Café da Manhã 01",
+"preco":"R$ 200,00",
+"image":"http://res.cloudinary.com/murilofrizanco/image/upload/v1471810625/cesta-cafe-manha-01_cohddx.jpg",
+"descricao":`
+Uma cesta de presentes ideal para presentar quem a gente gosta.
+
+* Produtos Diversos
+`
+},
+
+{
+"titulo":"Vinho + Bendito Cacao",
+"preco":"R$ 61,00",
+"image":"http://res.cloudinary.com/murilofrizanco/image/upload/v1471810623/vinho-bendito-cacao-barra_tzmmk6.jpg",
+"descricao":`
+Uma cesta de presentes ideal para presentar quem a gente gosta.
+
+* 1x Vinho Casa Valduga
+* 1x Barra Bendito Cacao 70% 100g
+* 4x Trufas 30g
+`
+},
+
+{
+"titulo":"Glamour",
+"preco":"R$ 23,00",
+"image":"http://res.cloudinary.com/murilofrizanco/image/upload/v1471810623/presente-glamour_hjecdq.jpg",
+"descricao":`
+Uma cesta de presentes ideal para presentar quem a gente gosta.
+
+* 1x Lata Glamour
+`
+},
+
+{
+"titulo":"Presente Simples 02",
+"preco":"R$ 6,00",
+"image":"http://res.cloudinary.com/murilofrizanco/image/upload/v1471810675/presente-03_sqb8lt.jpg",
+"descricao":`
+Uma cesta de presentes ideal para presentar quem a gente gosta.
+
+* 2x Trufas 30g
+`
+},
+
+];
